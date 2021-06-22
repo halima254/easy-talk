dc4e735b91c900fb3951f904b28b8a144d6d43b8
enock <enckkipronoh@gmail.com>
2021-06-22 12:05:49 +0300

    modified the html page

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,18 @@
  function togglePopup(){
     document.getElementById("popup-1").classList.toggle("active"); 
  } 
- function togglePopup1(){
-    document.getElementById("popup-12").classList.toggle("active"); 
+ function togglePopup2(){
+    document.getElementById("popup-2").classList.toggle("active"); 
+ } 
+ function togglePopup3(){
+    document.getElementById("popup-3").classList.toggle("active"); 
+ } 
+ function togglePopup4(){
+    document.getElementById("popup-4").classList.toggle("active"); 
+ } 
+ function togglePopup5(){
+    document.getElementById("popup-5").classList.toggle("active"); 
+ } 
+ function togglePopupi(){
+    document.getElementById("popup-i").classList.toggle("active"); 
  } 
