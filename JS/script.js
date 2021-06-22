24c22a742cdaaf237b9dc4c3f89e358a5b9c03db
enock <enckkipronoh@gmail.com>
2021-06-22 10:25:04 +0300

    added the banner

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+ function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active"); 
+ } 
+ function togglePopup1(){
+    document.getElementById("popup-12").classList.toggle("active"); 
+ } 
