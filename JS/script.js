6ec2106f320d46cfaa29e06a0317c94624fec2e7
enock <enckkipronoh@gmail.com>
2021-06-23 15:05:31 +0300

    validating the booking form

--- JS/script.js
+++ JS/script.js
@@ -16,3 +16,38 @@
  function togglePopupi(){
     document.getElementById("popup-i").classList.toggle("active"); 
  } 
+
+
+  // Form validation code will come here.
+  function validate(){  
+   var name=document.getElementById('A');  
+   var email=document.getElementById('B');
+   var password=document.getElementById('C')  
+   var location=document.getElementById('D')  
+     
+   if (name.value==""){  
+     alert("Name can't be blank");  
+     return false;
+     
+   }
+   // if(email.value==''){
+   //    alert('Input a valid email')
+   // }
+   if(password.value.length<6){  
+     alert("Password must be at least 6 characters long.");  
+     return false;  
+     }
+     if(location.value==''){
+      alert('Input for location cannot be empty')
+   }  
+     alert('Thank you for booking with us!')
+   }
+
+
+   // function validate(){
+   //    var name = parseInt(document.getElementById("A") .value);
+   //    if(name==null){
+   //        alert ("Name cannot be blank");
+   //    }
+   // }
+      
